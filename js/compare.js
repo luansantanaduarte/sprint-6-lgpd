24e1aa9a0b0fd7c63ab97b98b314eb46f6af9eed
luansantanaduarte <lsantana.duarte7@gmail.com>
2025-10-13 16:55:12 -0300

    finalizando página de lançamento

--- js/compare.js
+++ js/compare.js
@@ -3,10 +3,29 @@
 let carArr = [];
 
 class Car {
-   
-
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+    nome;
+    preco;
+    alturaCacamba;
+    alturaVeiculo;
+    alturaSolo;
+    capacidadeCarga;
+    motor;
+    potencia;
+    volumeCacamba;
+    roda;
+    image;
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image) {
+        this.nome = nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        this.image = image;
     }
 } 
 
@@ -20,13 +39,11 @@ function GetCarArrPosition(arr, carClass) {
 }
 
 function SetCarToCompare(el, carClass) {
-   
     if(carClass instanceof Car){       
         if(el.checked){
-                
-            
+            carArr.push(carClass);
         } else {
-          
+            carArr.pop(carArr)
         } 
     } else {
         throw "You need set a Car Class";
@@ -35,7 +52,10 @@ function SetCarToCompare(el, carClass) {
 
 function ShowCompare() {
     if(carArr.length < 2) {
-        alert("Precisa marcar 2 carros para apresentar a comparação");
+        alert("Precisa marcar 2 carros para apresentar a comparação.");
+        return;
+    } else if (carArr.length > 2) {
+        alert("Só é possível comparar 2 carros por vez.");
         return;
     }
 
@@ -44,9 +64,40 @@ function ShowCompare() {
 }
 
 function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
+    document.getElementById("compare").style.display = "none";
 }
 
 function UpdateCompareTable() {
+    document.getElementById("compare_image_0").innerHTML = `<img src="${carArr[0].image}" alt="${carArr[0].nome}">`;
+    document.getElementById("compare_image_1").innerHTML = `<img src="${carArr[1].image}" alt="${carArr[1].nome}">`;
+
+    document.getElementById("compare_modelo_0").innerHTML = `<p>${carArr[0].nome}</p>`;
+    document.getElementById("compare_modelo_1").innerHTML = `<p>${carArr[1].nome}</p>`;
     
+    document.getElementById("compare_alturacacamba_0").innerHTML = `<p>${carArr[0].alturaCacamba}</p>`;
+    document.getElementById("compare_alturacacamba_1").innerHTML = `<p>${carArr[1].alturaCacamba}</p>`; 
+
+    document.getElementById("compare_alturaveiculo_0").innerHTML = `<p>${carArr[0].alturaVeiculo}</p>`;
+    document.getElementById("compare_alturaveiculo_1").innerHTML = `<p>${carArr[1].alturaVeiculo}</p>`; 
+
+    document.getElementById("compare_alturasolo_0").innerHTML = `<p>${carArr[0].alturaSolo}</p>`;
+    document.getElementById("compare_alturasolo_1").innerHTML = `<p>${carArr[1].alturaSolo}</p>`;
+
+    document.getElementById("compare_capacidadecarga_0").innerHTML = `<p>${carArr[0].capacidadeCarga}</p>`;
+    document.getElementById("compare_capacidadecarga_1").innerHTML = `<p>${carArr[1].capacidadeCarga}</p>`;
+
+    document.getElementById("compare_motor_0").innerHTML = `<p>${carArr[0].motor}</p>`;
+    document.getElementById("compare_motor_1").innerHTML = `<p>${carArr[1].motor}</p>`;
+    
+    document.getElementById("compare_potencia_0").innerHTML = `<p>${carArr[0].potencia}</p>`;
+    document.getElementById("compare_potencia_1").innerHTML = `<p>${carArr[1].potencia}</p>`;
+
+    document.getElementById("compare_volumecacamba_0").innerHTML = `<p>${carArr[0].volumeCacamba}</p>`;
+    document.getElementById("compare_volumecacamba_1").innerHTML = `<p>${carArr[1].volumeCacamba}</p>`;
+
+    document.getElementById("compare_roda_0").innerHTML = `<p>${carArr[0].roda}</p>`;
+    document.getElementById("compare_roda_1").innerHTML = `<p>${carArr[1].roda}</p>`;
+
+    document.getElementById("compare_preco_0").innerHTML = `<p>${carArr[0].preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>`;
+    document.getElementById("compare_preco_1").innerHTML = `<p>${carArr[1].preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>`;
 }
