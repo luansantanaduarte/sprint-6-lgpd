44f5ff2ae7ebce460554f48f213e96de51411ef6
Luan <lsantana.duarte7@gmail.com>
2025-10-14 18:51:39 -0300

    exibindo mensagem na tela quando é enviado o formulário

--- js/form.js
+++ js/form.js
@@ -2,7 +2,14 @@
 //class contato
 
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
 }
 
 function Post(form) {
@@ -17,11 +24,9 @@ function Post(form) {
 }
 
 function Enviar() {
-
     var nome = document.getElementById("nomeid");
 
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
-
 }
