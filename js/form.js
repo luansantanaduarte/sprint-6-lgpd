dd8088f3e86e94fe2d8e0fe3921ef35663a14643
luansantanaduarte <lsantana.duarte7@gmail.com>
2025-10-27 16:46:02 -0300

    implementando lgpd e resetando o forms

--- js/form.js
+++ js/form.js
@@ -10,21 +10,39 @@ class Contato {
     }
 }
 
+const termosDeUso = document.getElementById("termosUso");
+const btnEnviar = document.querySelector("button");
+const mensagem = document.querySelector("abbr");
+
+termosDeUso.addEventListener("change", function() {
+    if (termosDeUso.checked) {
+        btnEnviar.classList.remove("disabled");
+        btnEnviar.removeAttribute("disabled");
+        mensagem.removeAttribute("title");
+    } else {
+        btnEnviar.classList.toggle("disabled");
+        btnEnviar.toggleAttribute("disabled", "true");
+        mensagem.setAttribute("title", "É necessário aceitar os termos de uso.")
+    }
+
+})
+
 function Post(form, event) {
     event.preventDefault();
-
+    
     let data = new Contato(form.elements.namedItem("nome").value,
-        form.elements.namedItem("sobrenome").value,
-        form.elements.namedItem("email").value,
-        form.elements.namedItem("cpf").value,
-        form.elements.namedItem("telefone").value,
-        form.elements.namedItem("contato").value);
-
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
+    form.elements.namedItem("sobrenome").value,
+    form.elements.namedItem("email").value,
+    form.elements.namedItem("cpf").value,
+    form.elements.namedItem("telefone").value,
+    form.elements.namedItem("contato").value);
+    
+    
+    const nome = document.getElementById("nomeid");
+    
+    if (nome.value != "" && termosDeUso.checked) {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        form.reset();
     }
     
     console.log(data);
